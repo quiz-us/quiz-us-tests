9800d0dc7f86a42d178fe93b858fa9edb29f6eaa
Joshua Ling <joshling1919@gmail.com>
2019-09-13 12:34:54 -0700

    First commit

---
+++ cypress/integration/teacher/login.spec.js
@@ -0,0 +1,20 @@
+beforeEach(() => {
+  cy.clearCookies();
+  cy.clearLocalStorage();
+  // deletes auth token, effectively logging user out:
+  indexedDB.deleteDatabase('localforage');
+});
+
+describe('login', () => {
+  it('can login', () => {
+    cy.visit('https://staging.quizus.org');
+    // it redirects to login:
+    cy.location('pathname').should('eq', '/login');
+    cy.get('input[type="email"]').type('test@example.com');
+    cy.get('input[type="password"]').type('wrongpassword{enter}');
+    cy.contains('Incorrect username and/or password. Please try again.').should('exist');
+
+    cy.get('input[type="password"]').clear();
+    cy.get('input[type="password"]').type('password1124{enter}');
+  });
+});
