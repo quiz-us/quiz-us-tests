6c37439768c787e475b209a7f251a06ec0348fc7
Joshua Ling <joshling1919@gmail.com>
2019-10-19 15:55:39 -0700

    Add student-client login specs

---
+++ cypress/integration/student/login.spec.js
@@ -0,0 +1,21 @@
+const { DUMMY_QR, STUDENT_STAGING } = Cypress.env();
+
+beforeEach(() => {
+  cy.clearCookies();
+  cy.clearLocalStorage();
+  // deletes auth token, effectively logging user out:
+  indexedDB.deleteDatabase('localforage');
+});
+
+describe('login', () => {
+  it('can log in using qr code', () => {
+    cy.visit(STUDENT_STAGING);
+    // it redirects to login:
+    cy.location('pathname').should('eq', '/login');
+
+    cy.contains('Use QR Login').click();
+    cy.get("input[name='qrTextInput'").type(DUMMY_QR);
+    cy.get("button[type='submit']").click();
+    cy.location('pathname').should('eq', '/');
+  });
+});
