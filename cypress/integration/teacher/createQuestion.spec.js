4a3c0d71deb48783a66638d7aa996cc6c22334d4
Joshua Ling <joshling1919@gmail.com>
2020-02-02 00:02:22 -0800

    Update createQuestion

--- cypress/integration/teacher/createQuestion.spec.js
+++ cypress/integration/teacher/createQuestion.spec.js
@@ -1,6 +1,10 @@
+const faker = require('faker');
+
 const { TEACHER_STAGING } = Cypress.env();
 
 describe('creating a question', () => {
+  const deckName = faker.random.uuid();
+  const questionText = 'What is the symbol for helium?';
   before(() => {
     cy.clearCookies();
     cy.clearLocalStorage();
@@ -8,11 +12,12 @@ describe('creating a question', () => {
     indexedDB.deleteDatabase('localforage');
     cy.visit(TEACHER_STAGING);
     cy.loginTeacher();
-  });
-  beforeEach(() => {
     cy.get("button[aria-label='menu']").click();
     cy.contains('Deck Creator').click({ force: true });
-    cy.location('pathname').should('eq', '/deck-creator');
+    cy.location('pathname').should('eq', '/decks/create');
+    cy.get('input#name').type(deckName);
+    cy.get('input#description').type('description');
+    cy.get("button[type='submit']").click();
   });
 
   it('can create a free response question', () => {
@@ -35,7 +40,7 @@ describe('creating a question', () => {
     // fill out question
     const questionEditor = cy.get("div[data-slate-editor='true']").first();
     questionEditor.click();
-    questionEditor.type('What is the symbol for helium?');
+    questionEditor.type(questionText);
     // fill out question
     const answerEditor = cy.get("div[data-slate-editor='true']").last();
     answerEditor.click();
@@ -45,5 +50,37 @@ describe('creating a question', () => {
     cy.contains('No cards in this deck yet').should('not.exist');
   });
 
-  // it('displays feedback if all required fields are not submitted');
+  it('adds the question to the deck', () => {
+    cy.contains('New question was added to the deck!').should('exist');
+    cy.reload();
+    cy.contains(questionText).should('exist');
+  });
+
+  it('can rename the deck', () => {
+    const updatedDeckName = faker.random.uuid();
+    cy.get("button[aria-label='Edit Deck']").click();
+    cy.get('input#name').clear();
+    cy.get('input#name').type(updatedDeckName);
+    cy.get('input#description').clear();
+    cy.get('input#description').type('updated description');
+    cy.get("button[type='submit'][form='deck-form']").click();
+    cy.contains(updatedDeckName).should('exist');
+  });
+
+  it('can remove the question from the deck', () => {
+    cy.get("button[aria-label='Remove From Current Deck']").click();
+    cy.contains(questionText).should('not.exist');
+    cy.reload();
+    cy.contains(questionText).should('not.exist');
+    cy.contains('No cards in this deck yet').should('exist');
+  });
+
+  it('can delete the deck', () => {
+    cy.get("button[aria-label='Delete Deck']").click();
+    cy.contains(
+      'Are you sure you want to delete this deck? This cannot be undone.'
+    ).should('exist');
+    cy.contains('Ok').click();
+    cy.location('pathname', { timeout: 10000 }).should('eq', '/');
+  });
 });
